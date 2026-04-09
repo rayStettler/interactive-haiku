4d87b94ceb471dee62110df7e2f5ecbfd31b4ec0
rayStettler <rps210001@utdallas.edu>
2026-04-09 19:05:50 +0000

    rotating again ig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,69 @@
 // main js
+
+let grid = document.querySelectorAll(".grid-container"); 
+//let divs = document.querySelection(".grid-container div"); 
+
+let rotation = 0;
+document.addEventListener("click", function () {
+    // let min = 10; 
+    // let max = 40; 
+    rotation += randomNumber(1, 200); 
+    
+    document.body.style.transform = `rotate(${rotation}deg)`; 
+}) 
+
+//let elements = document.querySelectorAll("body *"); 
+//console.log(elements.length); 
+//let divs = document.querySelection(".grid-container div"); 
+ 
+
+function randomize() {
+    
+    divs.forEach(function(div) {
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 360);
+        let rotate = randomNumber(0, 360);
+
+        if(Math.random() > .5) {
+            scale = 1; 
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80); 
+        }
+
+        div.style.transform = 
+            `scale(${scale})
+            translate(${translateX}%, ${translateY}%)
+            rotate(${rotate}deg)`; 
+
+        
+    }); 
+
+    console.log(divs.length, "randomize! "); 
+ 
+} 
+
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1) + min; 
+}
+
+/* 
 let grid = document.querySelectorAll(".grid-container"); 
 let divs = document.querySelection(".grid-container div"); 
 
-function randomize() {
-    console.log(divs.length, "randomize!");
+let rotation = 0;
+document.addEventListener("click", function () {
+    let min = 1;
+    let max = 20;
+    rotation += Math.random() * (max - min + 1) + min;
+    document.body.style.transform = `rotate(${rotation}deg)`;
+}) 
 
+
+function randomize() {
+    
     grid[0].styles.transform = `rotate(${Math.random() * 360}deg)`; 
 
     divs.forEach(function (div) {
@@ -18,9 +77,13 @@ function randomize() {
             translateX = randomNumber(0, 80);
             translateY = randomNumber(0, 80); 
         }
-        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+        
+        div.style.transform = 
+            `scale(${scale}) 
+            translate(${translateX}%, ${translateY}%) 
+            rotate(${rotate}deg)`;
     }); 
-
+    console.log(divs.length, "randomize!");
     
 }
 
@@ -29,55 +92,4 @@ document.addEventListener("click", randomize);
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
-
-//let rotation = 0;
-/*
-document.addEventListener("click", function () {
-    // let min = 10; 
-    // let max = 40; 
-    rotation += randomNumber(1, 200); 
-    
-    document.body.style.transform = `rotate(${rotation}deg)`; 
-}) 
 */ 
-//let elements = document.querySelectorAll("body *"); 
-//console.log(elements.length); 
-
-/*
-function randomize() {
-    console.log(divs.length, "randomize! "); 
-    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`; 
-
-    divs.forEach(function(div) {
-        let scale = randomNumber(.5, 1.2);
-        let translateX = randomNumber(0, 50);
-        let translateY = randomNumber(0, 360);
-        let rotate = randomNumber(0, 360);
-
-        if(Math.randam() > .5) {
-            scale = 1; 
-            translateX = randomNumber(0, 80);
-            translateY = randomNumber(0, 80); 
-        }
-
-        /* div.style.transform = 
-            `scale(${scale})
-            translate(${translateX}%, ${translateY}%)
-            rotate(${rotate}deg)`; 
-    div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}% rotate(${rotate}deg)`; 
-    
-    }); 
-   
- 
-} 
-
-document.addEventListener("click", randomize);
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + 1) + min; 
-}
-*/ 
-/*
-elements.forEach(function(ele) {
-	ele.style.transform = `rotate(${Math.random() * 360}deg)`;
-}); */ 
