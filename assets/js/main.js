19fa88052ef994873486517e1dac2125cde4e719
rayStettler <rps210001@utdallas.edu>
2026-04-09 18:06:54 +0000

    copy Megan 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 // main js
+let grid = document.querySelectorAll(".grid-container"); 
+let divs = document.querySelection(".grid-container div"); 
 
-let rotation = 0;
+//let rotation = 0;
+/*
 document.addEventListener("click", function () {
     // let min = 10; 
     // let max = 40; 
@@ -8,31 +11,44 @@ document.addEventListener("click", function () {
     
     document.body.style.transform = `rotate(${rotation}deg)`; 
 }) 
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + 1) + min; 
-}
+*/ 
 //let elements = document.querySelectorAll("body *"); 
 //console.log(elements.length); 
-let divs = document.querySelection(".grid-container div"); 
- 
+
 
 function randomize() {
-    
+    console.log(divs.length, "randomize! "); 
+    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`; 
+
     divs.forEach(function(div) {
         let scale = randomNumber(.5, 1.2);
         let translateX = randomNumber(0, 50);
         let translateY = randomNumber(0, 360);
         let rotate = randomNumber(0, 360);
-        div.style.transform = 
+
+        if(Math.randam() > .5) {
+            scale = 1; 
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80); 
+        }
+
+        /* div.style.transform = 
             `scale(${scale})
             translate(${translateX}%, ${translateY}%)
-            rotate(${rotate}deg)`; 
-    }) 
-
-    console.log(divs.length, "randomize! "); 
+            rotate(${rotate}deg)`; */ 
+    div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}% rotate(${rotate}deg)`; 
+    
+    }); 
+   
  
 } 
+
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1) + min; 
+}
+
 /*
 elements.forEach(function(ele) {
 	ele.style.transform = `rotate(${Math.random() * 360}deg)`;
